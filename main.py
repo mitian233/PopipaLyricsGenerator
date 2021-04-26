5a961445090c084b9097f7809deccea9a8647e09
Aaki阿湫 <mitian233@yahoo.co.jp>
2021-04-26 19:37:19 +0800

    Prototype

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import random
+from dictionary import meishi
+from dictionary import doushi
+from dictionary import jyoushi
+from dictionary import fukushi
 
+a = random.randint(1, 13)
+b = random.randint(1, 2)
+c = random.randint(0, 1)
+d = random.randint(1, 3)
+
+print(meishi[a] + jyoushi[b] + fukushi[c] + doushi[d])
